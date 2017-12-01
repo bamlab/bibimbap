42fb7543a1a6c2a6ce3f4c5ff8ba24aac87b6ca3
Arthur Levoyer <arthurl@bam.tech>
2017-12-01 15:57:47 +0100

    Add style

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
 import Page from './Page';
 
 export default class App extends React.Component {
   render() {
-    return <Page />;
+    return (
+      <Page style={styles.page}>
+        <TextInput style={styles.input} placeholder="Entrez votre nom" />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.textButton}>Validez</Text>
+        </TouchableOpacity>
+      </Page>
+    );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: '#ffd21a',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 16
+  },
+  input: {
+    alignSelf: 'stretch',
+    paddingVertical: 13,
+    backgroundColor: '#FCE696',
+    fontFamily: 'Arial',
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#9b9b9b',
+    marginBottom: 32
+  },
+  button: {
+    backgroundColor: 'white',
+    alignSelf: 'stretch',
+    paddingVertical: 12
+  },
+  textButton: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#4a4a4a'
+  }
+});
