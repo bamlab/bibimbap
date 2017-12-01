adaf229073624a2c4b5526c19c66f299fe845c1c
Laure Retru-Chavastel <laurerc@bam.tech>
2017-12-01 12:12:48 +0100

    Add Label, Input, Button and start to style them

--- form1/App.js
+++ form1/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
+import Page from './Page';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Page>
+        <Text style={styles.label}>BIBIMBAP</Text>
+        <TextInput style={styles.input}/>
+        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>C'est bien mon nom</Text></TouchableOpacity>
+      </Page>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  label: {color: '#48c89a'},
+  input: {},
+  button: {},
+  textButton: {} 
 });
