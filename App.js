b72ba98e8f7f760899f88a8f6f13708ee814aef9
Darius Afchar <darius.afchar@live.fr>
2017-12-01 16:22:27 +0100

    connect

--- App.js
+++ App.js
@@ -8,44 +8,30 @@ import {
 } from 'react-native';
 import Page from './Page';
 import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+
 
 const actionTypes = { saveInput: 'SAVE_INPUT'};
 
-const saveInputCreator = (value) => ({ type: actionTypes.saveInput, payload: value})
+const saveInputCreator = (value) => ({ type: actionTypes.saveInput, payload: value});
 
 const initialState = {
   currentName: '',
 };
 
 const reducer = (state = initialState, action) => {
+  console.log(state, action);
   switch (action.type) {
     case actionTypes.saveInput:
       return state.currentName;
   default:
     return state;
 }
-}
+};
 
 const getCurrentName = () => state.currentName;
 
 const store = createStore(reducer);
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <Page style={styles.page}>
-        <TextInput
-          placeholderTextColor="white"
-          placeholder="nom"
-          style={styles.input}
-        />
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Valider</Text>
-        </TouchableOpacity>
-      </Page>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   page: {
@@ -75,3 +61,40 @@ const styles = StyleSheet.create({
     textAlign: 'center'
   }
 });
+
+
+class MainPage extends React.Component {
+  render() {
+    return (
+      <Page style={styles.page}>
+        <TextInput
+          placeholderTextColor="white"
+          placeholder="nom"
+          style={styles.input}
+          onChangeText={this.props.setName}
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Valider</Text>
+        </TouchableOpacity>
+      </Page>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({});
+const mapDispatchToProps = {
+  setName: saveInputCreator,
+};
+
+
+const ConnectedMainPage = connect(mapStateToProps, mapDispatchToProps)(MainPage);
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <ConnectedMainPage />
+      </Provider>
+    );
+  }
+}
