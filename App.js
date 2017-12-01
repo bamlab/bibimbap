e7c022d651fbe665f9a60bc0d760e173910738cf
Arthur Levoyer <arthurl@bam.tech>
2017-12-01 15:06:14 +0100

    Add linear agrdient, button and textinput

--- App.js
+++ App.js
@@ -1,15 +1,55 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
 import Page from './Page';
 
 export default class App extends React.Component {
   render() {
-    return <Page style={styles.page} />;
+    return (
+      <Page style={styles.page}>
+        <TextInput
+          placeholderTextColor="white"
+          placeholder="nom"
+          style={styles.input}
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Valider</Text>
+        </TouchableOpacity>
+      </Page>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   page: {
-    paddingHorizontal: 16
+    flex: 1,
+    paddingHorizontal: 16,
+    backgroundColor: '#25c8f9'
+  },
+  input: {
+    alignSelf: 'stretch',
+    color: 'white',
+    paddingVertical: 5,
+    textAlign: 'center',
+    borderBottomColor: 'white',
+    borderBottomWidth: 2,
+    marginBottom: 30,
+    fontSize: 20
+  },
+  button: {
+    alignSelf: 'stretch',
+    backgroundColor: 'white',
+    paddingHorizontal: 6,
+    paddingVertical: 13
+  },
+  buttonText: {
+    color: '#177aed',
+    fontSize: 20,
+    textAlign: 'center'
   }
 });
